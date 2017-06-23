e92a454cf6be7318cc3070d392adc87555e1af8d
takuya.kikuchi <takuya.kikuchi@i-studio.co.jp>
2017-06-23 19:02:31 +0900

    gulp改造

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-// 共通
+/* -----------------------------------------
+共通
+----------------------------------------- */
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
 var notify = require("gulp-notify");
-// html
+var rename = require("gulp-rename")
+/* -----------------------------------------
+html
+----------------------------------------- */
 var htmlhint = require('gulp-htmlhint');
 var pug = require('gulp-pug');
-// Sass
+/* -----------------------------------------
+Sass
+----------------------------------------- */
 var sass = require('gulp-sass');
 var minify = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
@@ -15,10 +22,15 @@ var concat = require('gulp-concat');
 var ssi = require('connect-ssi');
 var pleeease = require('gulp-pleeease');
 var csscomb = require('gulp-csscomb');
-// JS
+var rename = require('gulp-rename');
+/* -----------------------------------------
+JS
+----------------------------------------- */
 var uglify = require("gulp-uglify");
 var babel = require("gulp-babel");
-// image
+/* -----------------------------------------
+image
+----------------------------------------- */
 var clean = require('gulp-clean');
 var vinylPaths = require('vinyl-paths');
 var changed = require('gulp-changed');
@@ -26,113 +38,125 @@ var imagemin = require('gulp-imagemin');
 var pngquant  = require('imagemin-pngquant');
 var cached = require('gulp-cached');
 var del = require('del');
-
+/* -----------------------------------------
+root path
+----------------------------------------- */
 var path = {
-  dist: 'htdocs/assets/',
-  top: 'htdocs/',
-  dev: 'htdocs_dev/assets/',
-  pub: 'htdocs_dev/'
+  pub: 'htdocs/',
+  dev: 'htdocs_dev/'
 }
-
-// BrowserSync
-gulp.task("browserSync", function() {
+/* -----------------------------------------
+*
+    BrowserSync
+*
+----------------------------------------- */
+gulp.task("browserSync", function () {
   browserSync({
-    notify: false,
+    notify: true,
     server: {
-      baseDir: path.pub,
+      baseDir: 'htdocs/'
     },
     open: 'external',
     startPath: '/',
     middleware: [
     ssi({
-      baseDir: path.pub,
+      baseDir: "htdocs/",
       ext: ".html"
     })
     ]
   });
-  gulp.watch(path.pub + '**/*', function() {
-    browserSync.reload();
-  });
 });
-
-// html
-gulp.task('html', function() {
-  console.log('ok');
-  gulp.src(path.pub + '**/*.html')
-  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-  .pipe(htmlhint())
-  .pipe(gulp.dest(path.top + ''))
-})
-
-// Sass
+/* -----------------------------------------
+*
+    reload
+*
+----------------------------------------- */
+gulp.task('reload', function(){
+  browserSync.reload();
+});
+/* -----------------------------------------
+*
+    html
+*
+----------------------------------------- */
+gulp.task('html', function(){
+  return gulp.src(path.dev + '**/*.html')
+  .pipe(plumber({
+    errorHandler: notify.onError("Error: <%= error.message %>")
+  }))
+  .pipe(gulp.dest(path.pub))
+});
+/* -----------------------------------------
+*
+    Sass
+*
+----------------------------------------- */
 gulp.task('scss', function() {
-  gulp.src(path.dev + 'sass/**/*.scss')
-  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-  .pipe(cached('scss'))
+  return gulp.src(path.dev + '**/*.scss')
+  .pipe(plumber({
+    errorHandler: notify.onError("Error: <%= error.message %>")
+  }))
   .pipe(csscomb())
-  .pipe(sass({
-    outputStyle: 'expanded'
+  .pipe(sass())
+  .pipe(rename(function (path) {
+    path.dirname = path.dirname.replace(/scss/g, 'css');
   }))
   .pipe(autoprefixer({
     browsers: ["last 2 versions", "Android >= 4","ios_saf >= 8", "ie >= 9"]
   }))
-  .pipe(gulp.dest(path.dev + 'css'))
   .pipe(minify())
-  .pipe(gulp.dest(path.dist + 'css'))
+  .pipe(gulp.dest(path.pub))
 });
-
-// del
+/* -----------------------------------------
+*
+    del
+*
+----------------------------------------- */
 gulp.task('clean', function() {
-  del([path.dist + 'img/**/*']);
+  del([path.dev + '**/images/*']);
 });
-
-// images
+/* -----------------------------------------
+*
+    images
+*
+----------------------------------------- */
 gulp.task('image', function() {
-  gulp.src(path.dev + 'img/**/*')
-  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+  return gulp.src(path.dev + '**/images/*')
+  .pipe(plumber({
+    errorHandler: notify.onError("Error: <%= error.message %>")
+  }))
   .pipe(cached('image'))
-  .pipe(changed(path.dist + 'img/**/*'))
+  .pipe(changed(path.pub + '**/images/*'))
   .pipe(imagemin({
     use: [pngquant()],
     progressive: true,
     interlaced: true
   }))
-  .pipe(gulp.dest(path.dist + 'img'))
+  .pipe(gulp.dest(path.pub))
 });
-
-// JS
+/* -----------------------------------------
+*
+    JS
+*
+----------------------------------------- */
 gulp.task('js', function() {
-  gulp.src(path.dev + 'js/**/*.js')
-  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-  .pipe(babel())
-  .pipe(uglify())
-  .pipe(gulp.dest(path.dist + 'js'))
-});
-
-// pug
-gulp.task('pug', function() {
-  gulp.src(path.pub + 'pug/**/*.pug')
-  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-  .pipe(pug({
-    pretty: true
+  return gulp.src(path.dev + '**/*.js')
+  .pipe(plumber({
+    errorHandler: notify.onError("Error: <%= error.message %>")
   }))
-  .pipe(gulp.dest(path.top + ''));
+  .pipe(uglify())
+  .pipe(gulp.dest(path.pub))
 });
-
-gulp.task('copyDev', function() {
-  gulp.src([path.dist + 'css/*'])
-  .pipe(gulp.dest(path.top + 'css'));
-  gulp.src([path.dist + 'image/*'])
-  .pipe(gulp.dest(path.top + 'image'));
-  gulp.src([path.dist + 'js/*'])
-  .pipe(gulp.dest(path.top + 'js'));
-});
-
+/* -----------------------------------------
+*
+    watch
+*
+----------------------------------------- */
 gulp.task('watch', function() {
-  gulp.watch(path.pub + '**/*.html',['html'])
-  gulp.watch(path.pub + 'pug/**/*.pug',['pug']);
-  gulp.watch(path.dev + 'sass/**/*.scss',['scss']);
-  gulp.watch(path.dev + 'js/**/*.js',['js']);
-  gulp.watch(path.dev + 'img/**/*',['image']);
+  gulp.watch(path.dev + '**/*.html',['reload', 'html']);
+  gulp.watch(path.dev + '**/*.scss',['reload', 'scss']);
+  gulp.watch(path.dev + '**/*.js',['reload', 'js']);
+  gulp.watch(path.dev + '**/images/*',['image']);
 });
-gulp.task('default', ['watch', 'browserSync', 'clean', 'image']);
+
+gulp.task('default', ['watch', 'browserSync']);
